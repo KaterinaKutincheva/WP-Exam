6d5211027657f27ec5ee9c8efefc19690e6c0a1d
Katerina Kutincheva <k.g.kutincheva@gmail.com>
2024-12-19 14:23:11 +0200

    Make dynamic footer, clear unnecessary icons and info

---
+++ wp-content/themes/a-world-1.0.0/footer.php
@@ -0,0 +1,32 @@
+<!-- footer section start -->
+<div class="footer_section layout_padding">
+   <div class="container">
+      <div class="input_btn_main">
+         <input type="email" class="mail_text" placeholder="Enter your email" name="email">
+         <div class="subscribe_bt"><a href="/subscribe">Subscribe</a></div>
+      </div>
+      <div class="location_main">
+         <div class="call_text"><img src="<?php echo get_theme_file_uri(); ?>/images/mail-icon.png" alt="Mail"></div>
+         <div class="call_text"><a href="mailto:info@sfcdaunbe.com">info@sfcdaunbe.com</a></div>
+      </div>
+      <div class="social_icon">
+         <ul>
+            <li><a href="https://facebook.com"><img src="<?php echo get_theme_file_uri(); ?>/images/fb-icon.png" alt="Facebook"></a></li>
+         </ul>
+      </div>
+   </div>
+</div>
+<!-- footer section end -->
+
+<!-- copyright section start -->
+<div class="copyright_section">
+   <div class="container">
+      <p class="copyright_text">2024 All Rights Reserved. Design by <a href="https://html.design">Free HTML Templates</a> Distributed by <a href="https://themewagon.com">ThemeWagon</a></p>
+   </div>
+</div>
+<!-- copyright section end -->
+
+<!-- Enqueueing Scripts in footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
